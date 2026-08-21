3f65fa1dda8eea6c65d45837fd28e073af316219
YourGitHubUsername <owenhaggerty@Owens-MacBook-Air.local>
2026-08-21 11:59:54 -0400

    Increment iOS build number to 2

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ module.exports = {
     scheme: 'saveitgolf',
     ios: {
       bundleIdentifier: 'com.saveitgolf.app',
-      buildNumber: '1',
+      buildNumber: '2',
       supportsTablet: true,
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
