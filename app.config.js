57bf65c33d0e279cfeaeeba719d46aa10d0e5111
YourGitHubUsername <owenhaggerty@Owens-Air.business.local>
2026-09-03 11:37:26 -0400

    Build 5 map fixes iPad compatibility

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ module.exports = {
     scheme: 'saveitgolf',
     ios: {
       bundleIdentifier: 'com.saveitgolf.app',
-      buildNumber: '4',
+      buildNumber: '5',
       supportsTablet: true,
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
