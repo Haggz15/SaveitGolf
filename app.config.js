1123b89355618fecf705e661d9ba51307c841b84
YourGitHubUsername <owenhaggerty@Owens-MacBook-Air.local>
2026-09-15 17:45:28 -0400

    Bump version to 1.0.1 build 9

--- app.config.js
+++ app.config.js
@@ -12,7 +12,7 @@ module.exports = {
     name: 'SaveitGolf',
     slug: 'SaveitGolf',
     owner: 'saveitgolf',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'dark',
